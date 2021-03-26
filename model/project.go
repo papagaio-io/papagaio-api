fe359493415f13b4d0afad7ae436a6389c78902f
alessandro.pinna <alessandro.pinna@sorint.it>
2021-03-26 14:39:38 +0100

    Refactoring

--- model/project.go
+++ model/project.go
@@ -11,9 +11,11 @@ type Project struct {
 	//Agola run info. Use branch in key map
 	LastBranchRunFailsMap map[string]RunInfo `json:"lastBranchRunMap"`
 	LastRun               RunInfo            `json:"lastRun"`
+	OlderRunFaild         RunInfo            `json:"olderRunFaild"`
 }
 
 type RunInfo struct {
-	RunID        string    `json:"runID"`
+	ID           string    `json:"id"`
+	Branch       string    `json:"branch"`
 	RunStartDate time.Time `json:"runStartDate"`
 }

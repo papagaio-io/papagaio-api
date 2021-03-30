b182693691ddb1441cc7d06fc45ff259860a21a0
alessandro.pinna <alessandro.pinna@sorint.it>
2021-03-30 15:37:33 +0200

    added log

--- controller/routing.go
+++ controller/routing.go
@@ -149,7 +149,7 @@ func handleRestrictedUserRoutes(h http.Handler) http.Handler {
 
 		user, err := db.GetUserByEmail(claim.Email)
 		if user == nil {
-			log.Println("User not found")
+			log.Println("User", claim.Email, "not found")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}

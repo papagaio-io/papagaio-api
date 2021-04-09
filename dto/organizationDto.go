900aa731ced869b3b297f376b14b4c790d709251
alessandro.pinna <alessandro.pinna@sorint.it>
2021-04-09 14:58:57 +0200

    dashboardManager fix

--- dto/organizationDto.go
+++ dto/organizationDto.go
@@ -5,12 +5,6 @@ type OrganizationDto struct {
 	Name       string         `json:"name"`
 	Visibility VisibilityType `json:"visibility"`
 
-	BehaviourInclude string        `json:"behaviourInclude"`
-	BehaviourExclude string        `json:"behaviourExclude"`
-	BehaviourType    BehaviourType `json:"behaviourType"`
-
-	ExternalUsers map[string]bool `json:"externalUsers,omitempty"`
-
 	Projects    []ProjectDto `json:"projects"`
 	WorstReport *ReportDto   `json:"worstReport,omitempty"`
 }
--- service/organizationService.go
+++ service/organizationService.go
@@ -248,7 +248,7 @@ func (service *OrganizationService) GetProjectReport(w http.ResponseWriter, r *h
 	if organization.Projects == nil {
 		NotFoundResponse(w)
 		return
-	} else if _, ok := organization.Projects[projectName]; !ok {
+	} else if project, ok := organization.Projects[projectName]; !ok || project.Archivied {
 		NotFoundResponse(w)
 		return
 	}

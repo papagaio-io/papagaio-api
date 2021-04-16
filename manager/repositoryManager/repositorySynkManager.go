088c2138854ed0ddb1659e6b1c621f663e62b5e7
alessandro.pinna <alessandro.pinna@sorint.it>
2021-04-16 11:38:28 +0200

    CheckoutAllGitRepository fix

--- manager/repositoryManager/repositorySynkManager.go
+++ manager/repositoryManager/repositorySynkManager.go
@@ -45,6 +45,9 @@ func CheckoutAllGitRepository(db repository.Database, organization *model.Organi
 
 		project := model.Project{OrganizationID: organization.ID, GitRepoPath: repo, AgolaProjectID: projectID}
 		organization.Projects[repo] = project
+
+		BranchSynck(db, gitSource, organization, repo)
+
 		db.SaveOrganization(organization)
 
 		log.Println("End add repository:", repo)

450bde4d8b2887b44cba1ce78a3918e9dc70f67c
Tullio <tbottiglieri@sorint.it>
2021-04-23 15:50:17 +0200

    add project url

--- utils/agolaUtils.go
+++ utils/agolaUtils.go
@@ -5,3 +5,7 @@ import "wecode.sorint.it/opensource/papagaio-api/config"
 func GetOrganizationUrl(organizationName string) string {
 	return config.Config.Agola.AgolaAddr + "/org/" + organizationName
 }
+
+func GetProjectUrl(organizationName string, projectName string) string {
+	return config.Config.Agola.AgolaAddr + "/org/" + organizationName + "/projects/" + projectName + ".proj"
+}

216c6026691b53b67c152a2f66f67e576f34eb81
alessandro.pinna <alessandro.pinna@sorint.it>
2021-04-27 15:48:19 +0200

    Added test TestGetOrganizationsOK

--- test/datas.go
+++ test/datas.go
@@ -2,11 +2,31 @@ package test
 
 import "wecode.sorint.it/opensource/papagaio-api/model"
 
-func MakeGitSourceList() *map[string]model.GitSource {
+func MakeGitSourceMap() *map[string]model.GitSource {
 	retVal := make(map[string]model.GitSource)
 
 	retVal["gitea"] = model.GitSource{Name: "gitea", GitType: model.Gitea}
 	retVal["github"] = model.GitSource{Name: "github", GitType: model.Github}
+
+	return &retVal
+}
+
+func MakeOrganizationMap() *map[string]model.Organization {
+	retVal := make(map[string]model.Organization)
+
+	//TODO
+	//retVal["Organization1"] = model.
+
+	return &retVal
+}
+
+func MakeOrganizationList() *[]model.Organization {
+	organizationMap := MakeOrganizationMap()
+	retVal := make([]model.Organization, 0)
+
+	for _, organization := range *organizationMap {
+		retVal = append(retVal, organization)
+	}
 
 	return &retVal
 }

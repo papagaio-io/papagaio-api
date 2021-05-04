7e97c0f30f1125602a7e627cd0d58cbbf065f48b
Tullio <tbottiglieri@sorint.it>
2021-05-04 14:35:15 +0200

    add unit tests for add and remove external user

--- test/api_organization_test.go
+++ test/api_organization_test.go
@@ -75,3 +75,72 @@ func TestAddExternalUser(t *testing.T) {
 	assert.Equal(t, resp.StatusCode, http.StatusOK, "http StatusCode is not OK")
 
 }
+func TestAddExternalUserWhenOrganizationNotFound(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	commonMutex := utils.NewEventMutex()
+	db := mock_repository.NewMockDatabase(ctl)
+	agolaApi := mock_agola.NewMockAgolaApiInterface(ctl)
+
+	serviceOrganization := service.OrganizationService{
+		Db:          db,
+		AgolaApi:    agolaApi,
+		CommonMutex: &commonMutex,
+	}
+	user := model.User{Email: "user@email.com"}
+
+	organizationRefTest := "testnotfound"
+	db.EXPECT().GetOrganizationByAgolaRef(gomock.Eq(organizationRefTest)).Return(nil, nil)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/{organizationName}", serviceOrganization.AddExternalUser)
+	ts := httptest.NewServer(router)
+
+	client := ts.Client()
+
+	data, _ := json.Marshal(user)
+	requestBody := strings.NewReader(string(data))
+	resp, err := client.Post(ts.URL+"/"+organizationRefTest, "application/json", requestBody)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, resp.StatusCode, http.StatusNotFound, "http StatusCode is not OK")
+}
+
+func TestRemoveExternalUserOk(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	commonMutex := utils.NewEventMutex()
+	db := mock_repository.NewMockDatabase(ctl)
+	agolaApi := mock_agola.NewMockAgolaApiInterface(ctl)
+
+	serviceOrganization := service.OrganizationService{
+		Db:          db,
+		AgolaApi:    agolaApi,
+		CommonMutex: &commonMutex,
+	}
+	mail := "user@email.com"
+	user := model.User{Email: mail}
+
+	org := (*MakeOrganizationList())[0]
+	org.ExternalUsers = make(map[string]bool)
+	org.ExternalUsers[mail] = true
+
+	db.EXPECT().GetOrganizationByAgolaRef(org.AgolaOrganizationRef).Return(&org, nil)
+	db.EXPECT().SaveOrganization(gomock.Any()).Return(nil)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/{organizationName}", serviceOrganization.RemoveExternalUser)
+	ts := httptest.NewServer(router)
+
+	client := ts.Client()
+
+	data, _ := json.Marshal(user)
+	requestBody := strings.NewReader(string(data))
+	resp, err := client.Post(ts.URL+"/"+org.AgolaOrganizationRef, "application/json", requestBody)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "http StatusCode is not OK")
+	exist := org.ExternalUsers[mail]
+	assert.Check(t, !exist, "")
+}

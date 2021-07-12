6fb2fb01b93d8095c58229e3bb9d27304a8fa1b0
alessandro.pinna <alessandro.pinna@sorint.it>
2021-07-12 17:50:07 +0200

    git GetUserInfo refactoring

--- service/oauth2Service.go
+++ service/oauth2Service.go
@@ -146,7 +146,6 @@ func (service *Oauth2Service) Callback(w http.ResponseWriter, r *http.Request) {
 		InternalServerError(w)
 	}
 
-	userInfo.UserPageURL = gitSource.GitAPIURL + "/" + userInfo.Login
 	response := dto.OauthCallbackResponseDto{Token: userToken, UserID: *user.UserID, UserInfo: *userInfo}
 	JSONokResponse(w, response)
 

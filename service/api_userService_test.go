4c9ea674031213e939fa73ab2f6c3a83e0d36f54
alessandro.pinna <alessandro.pinna@sorint.it>
2021-07-26 12:16:35 +0200

    added test

--- service/api_userService_test.go
+++ service/api_userService_test.go
@@ -54,3 +54,53 @@ func TestChangeUserRoleOK(t *testing.T) {
 	assert.Equal(t, err, nil)
 	assert.Equal(t, resp.StatusCode, http.StatusOK, "http StatusCode is not OK")
 }
+
+func TestChangeUserRoleNotFound(t *testing.T) {
+	setupUserMock(t)
+
+	user := test.MakeUser()
+	request := dto.ChangeUserRoleRequestDto{
+		UserID:   utils.NewUint64(*user.UserID),
+		UserRole: dto.Administrator,
+	}
+
+	db.EXPECT().GetUserByUserId(*user.UserID).Return(nil, nil)
+
+	data, _ := json.Marshal(request)
+	requestBody := strings.NewReader(string(data))
+
+	router := test.SetupBaseRouter(nil)
+	router.HandleFunc("/changeuserrole", serviceUser.ChangeUserRole)
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	client := ts.Client()
+	resp, err := client.Post(ts.URL+"/changeuserrole", "application/json", requestBody)
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, resp.StatusCode, http.StatusInternalServerError, "http StatusCode is not correct")
+}
+
+func TestChangeUserRoleRequestNotValid(t *testing.T) {
+	setupUserMock(t)
+
+	user := test.MakeUser()
+	request := dto.ChangeUserRoleRequestDto{
+		UserID:   utils.NewUint64(*user.UserID),
+		UserRole: "guest",
+	}
+
+	data, _ := json.Marshal(request)
+	requestBody := strings.NewReader(string(data))
+
+	router := test.SetupBaseRouter(nil)
+	router.HandleFunc("/changeuserrole", serviceUser.ChangeUserRole)
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	client := ts.Client()
+	resp, err := client.Post(ts.URL+"/changeuserrole", "application/json", requestBody)
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, resp.StatusCode, http.StatusInternalServerError, "http StatusCode is not correct")
+}
